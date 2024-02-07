02b0a8b48397806a6f8e15d6b5d5522600cd4f77
rn-santos227 <rn_santos227@yahoo.com>
2024-02-07 23:35:45 +0800

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    arbsep: {
+      url: process.env.VITE_JSON_RPC_URL!,
+      accounts: [
+        process.env.VITE_PRIVATE_KEY!
+      ],
+    },
+  },
 };
 
 export default config;
